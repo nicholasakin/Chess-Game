f4db688625f9abef37fe6744b0d692d98f3b704c
Nicky <nicholasakin@gmail.com>
2021-01-08 20:12:03 -0500

    Commented lobby and game scene creation.

--- Chess Game/src/game/Game.java
+++ Chess Game/src/game/Game.java
@@ -10,7 +10,7 @@ import javafx.scene.text.Text;
 
 public class Game extends Application {
 	
-	VBox lobby;
+	
 	Scene lobbyScene;
 	Scene gameScene;
 	
@@ -19,9 +19,9 @@ public class Game extends Application {
 		
 		
 		
-		makeLobby(stage);
 		
-		lobbyScene = new Scene(lobby);
+		
+		lobbyScene = makeLobby(stage);
 	    gameScene = makeGame(stage);
 		
 		
@@ -36,27 +36,50 @@ public class Game extends Application {
 	 * Helper method used to create lobby for game.
 	 * Lobby allows players to wait and click 'play' to start the game.
 	 * The 'play' button transitions the scene to the game scene.
+	 * @param stage - Main stage of the application. Taken in to have the lobby scene applied.
+	 * @return lobbyScene - Scene representing the lobby of the game
 	 */
-	public void makeLobby(Stage stage) {
+	public Scene makeLobby(Stage stage) {
+		//button that transitions the lobbh scene to the game scene
 		Button play = new Button("Play");
 		
+		//buttons action to set the scene of the main stage
 		play.setOnAction(e -> {
 			System.out.println("Switching scene");
 			stage.setScene(gameScene);
 		});
 		
-		lobby = new VBox(play);
+		//main container of the lobby
+		VBox lobby = new VBox(play);
+		Scene lobbyScene = new Scene(lobby);
+		
+		//returning the lobby scene
+		return lobbyScene;
 		
 	} //make lobby
 	
+	/**
+	 * Helper method used to create game scene for game.
+	 * Game allows two players to play the chess game. 
+	 * The players can also return to the lobby to reset the game. 
+	 * The 'play' button transitions the scene to the game scene.
+	 * @param stage - Main stage of the application. Taken in to have the lobby scene applied.
+	 * @return gameScene - Scene representing the main game area. 
+	 */
 	public Scene makeGame(Stage stage) {
+		//text representing the color of who's turn
 		Text turn = new Text("Turn: White");
+		
+		//main container in game scene
 		VBox root = new VBox(turn);
 		Scene gameScene = new Scene(root);
+		
+		//scene is returned 
 		return gameScene;
 		
-		
 	} //makeGame
+	
+	
 	
 	
 } //class
